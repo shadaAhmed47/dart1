ea627dc631a9e5f079bc92df48c3c6310d0386b5
shadaAhmed47 <115115874+shadaAhmed47@users.noreply.github.com>
2024-03-06 23:18:03 +0200

    Add files via upload

---
+++ first_project/lib/firstfile/first.dart
@@ -0,0 +1,20 @@
+import "dart:math";
+void main(List<String>arguments) {
+  // task 1
+  num n = pow(2, 2);
+  num area = 3.14 * (n);
+  print(area);
+
+  //task2
+  num f=50;
+  num c=5/9*(f-32);
+  print(c);
+
+  //task3
+  double a=20.5;
+  double b=30;
+  double d=10;
+  num average=0;
+  average=(a+b+c)/3;
+  print(average);
+}
